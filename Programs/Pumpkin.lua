88fddbc83fed3c6ad174626a95eaf4597fb0eb80
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-04-29 16:15:01 -0400

    Pumpkins

--- Programs/Pumpkin.lua
+++ Programs/Pumpkin.lua
@@ -1,5 +1,5 @@
 
-function check_if_fully_grown()
+function IsPumpkinBelow()
 	local success, data = turtle.inspectDown()
 	--if success then
 	--	return data.state.age == 7
@@ -14,80 +14,161 @@ function check_if_fully_grown()
 end
 
 
-local width = 12
-local height = 21
+--region Navigation
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
 
-while true do
-	for i=0,width do
-		for ii=0, height do
-			if check_if_fully_grown() then
-				turtle.digDown()
-				--turtle.placeDown()
-			end
-			turtle.forward()
-		end
-		turtle.turnRight()
-		turtle.forward()
-		turtle.turnRight()
-		for ii=0, height do
-			if check_if_fully_grown() then
-				turtle.digDown()
-				--turtle.placeDown()
-			end
-			turtle.forward()
-		end
-		turtle.turnLeft()
-		turtle.forward()
-		turtle.turnLeft()
-	end
+StoppedPos = {x = 0, y = 0, z = 0, dir = NORTH}
+LocalPos = {x = 0, y = 0, z = 0, dir = NORTH}
 
-	turtle.turnLeft()
-	for i = 0, width * 2 do
-		turtle.forward()
-	end
-	turtle.turnRight()
-
-	turtle.turnRight()
-	turtle.turnRight()
-
-	for i = 1, 16 do
-		turtle.select(i)
-		turtle.drop()
-	end
-
-	turtle.turnRight()
-	turtle.turnRight()
-
-	os.sleep(60 * 60)
+-- dir 0 = N
+--         +z
+-- dir 1 = E
+--         +x
+-- dir 2 = S
+--         -z
+-- dir 3 = W
+--         -x
+function Forward()
+    turtle.forward()
+    if LocalPos.dir == NORTH then
+        LocalPos.z = LocalPos.z + 1
+    elseif LocalPos.dir == EAST then
+        LocalPos.x = LocalPos.x + 1
+    elseif LocalPos.dir == SOUTH then
+        LocalPos.z = LocalPos.z - 1
+    elseif LocalPos.dir == WEST then
+        LocalPos.x = LocalPos.x - 1
+    else
+        print("ut oh ", LocalPos.dir, " is fucky wucky")
+    end
 end
 
-print(check_if_fully_grown())
+function Left()
+    turtle.turnLeft()
+    LocalPos.dir = LocalPos.dir - 1
+    LocalPos.dir = LocalPos.dir % 4
+end
 
---turtle.digDown()
---turtle.placeDown()
+function Right()
+    turtle.turnRight()
+    LocalPos.dir = LocalPos.dir + 1
+    LocalPos.dir = LocalPos.dir % 4
+end
+
+function Up()
+    turtle.up()
+    LocalPos.y = LocalPos.y + 1
+end
+
+function Down()
+    turtle.down()
+    LocalPos.y = LocalPos.y - 1
+end
+
+function FaceDirection(dir)
+    while dir ~= LocalPos.dir do
+        Right()
+    end
+end
+
+function GoToLocation(x, y, z)
+    if y > LocalPos.y then
+        for i = 0, math.abs(LocalPos.y - y) - 1, 1 do
+            Up()
+        end
+    elseif y < LocalPos.y then
+        for i = 0, math.abs(LocalPos.y - y) - 1, 1 do
+            Down()
+        end
+    end
+
+    if z > LocalPos.z then
+        FaceDirection(NORTH)
+    elseif z < LocalPos.z then
+        FaceDirection(SOUTH)
+    end
+
+    if z ~= LocalPos.z then
+        for i = 0, math.abs(LocalPos.z - z) - 1, 1 do
+            Forward()
+        end
+    end
+
+    if x > LocalPos.x then
+        FaceDirection(EAST)
+    elseif x < LocalPos.x then
+        FaceDirection(WEST)
+    end
+
+    if x ~= LocalPos.x then
+        for i = 0, math.abs(LocalPos.x - x) - 1, 1 do
+            Forward()
+        end
+    end
+
+    FaceDirection(NORTH)
+end
+
+function SaveStoppingPoint()
+    StoppedPos.x = LocalPos.x
+    StoppedPos.y = LocalPos.y
+    StoppedPos.z = LocalPos.z
+    StoppedPos.dir = LocalPos.dir
+end
+
+function PrintPos()
+    print("Pos: ", LocalPos.x, ", ", LocalPos.y, ", ", LocalPos.z)
+end
+--endregion
+
+function FarmPath(dist)
+    for i = 0, dist - 1, 1 do
+        if IsPumpkinBelow() then
+			turtle.digDown()
+		end
+        Forward()
+    end
+end
+
+function FarmLayer(length, width)
+    for w = 0, (width / 2) - 1, 1 do
+        -- dig in line of length
+        FarmPath(length)
+        -- turn right
+        Right()
+        Forward()
+        Right()
+        FarmPath(length)
+        -- turn left
+        Left()
+        Forward()
+        Left()
+    end
+    if width % 2 == 1 then
+        FarmPath(length)
+    end
+end
+
+function Depot()
+    for i = 16, 1, -1 do
+        turtle.select(i)
+        turtle.dropDown()
+    end
+end
 
 Length, Width = ...
 
 function Main()
-	print("Starting layer ", i)
-	print("Going to 0, 0 and y = ", -i * 3)
-	GoToLocation(0, -i * 3, 0)
-	DigLayer(Length, Width)
-	if ShouldReturn() then
-		GoToLocation(0, 0, 0)
-		Depot()
-		Refuel()
-	end
-	GoToLocation(0, -i * 3, 0)
-	if i ~= depthPasses - 1 then
-		Down()
-		DigAround()
-		Down()
-		DigAround()
-		Down()
-		DigAround()
-	end
-
+    while true do
+        Forward()
+        FarmLayer(Length, Width)
+        GoToLocation(0, 0, 0)
+        Depot()
+        sleep(900)
+    end
 end
 
 Main()
